b788179e74b564a47cbb098b299947e1ebd18f8e
Erick <erick.nahuelpaiva.001@gmail.com>
2024-11-28 17:41:42 -0300

    agregada verificacion para emails
    solo verifica gmails

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,12 +5,13 @@ import {
   updateUser,
   deleteUser,
 } from '../controllers/userController.js';
+import { validateGmail } from '../middlewares/validateEmail.js';
 
 const router = express.Router();
 
 router.get('/user', getAllUsers);        // Obtener todos los usuarios
-router.post('/user', createUser);        // Crear un usuario
-router.put('/user/:id', updateUser);      // Actualizar un usuario por ID
+router.post('/user', validateGmail, createUser);        // Crear un usuario
+router.put('/user/:id',validateGmail, updateUser);      // Actualizar un usuario por ID
 router.delete('/user/:id', deleteUser);  // Eliminar un usuario por ID
 
 export default router;
